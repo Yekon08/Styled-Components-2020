2a198c8f3319fa1aa2fed25ec639907038749268
Yekon08 <max1901@live.fr>
2020-10-05 16:46:56 +0200

    Section & Footer

--- src/components/infoSection/InfoSection.js
+++ src/components/infoSection/InfoSection.js
@@ -1,15 +1,54 @@
 import React from "react";
-import { InfoSec } from "./InfoSection.elements";
-import { Container } from "../../globalStyles";
+import { Link } from "react-router-dom";
+import { Container, Button } from "../../globalStyles";
+import {
+  InfoSec,
+  InfoRow,
+  InfoColumn,
+  TextWrapper,
+  TopLine,
+  Heading,
+  SubTitle,
+  ImgWrapper,
+  Img,
+} from "./InfoSection.elements";
 
-const InfoSection = ({ lightBg }) => {
+const InfoSection = ({
+  primary,
+  lightBg,
+  imgStart,
+  lightTopLine,
+  lightTextDesc,
+  buttonLabel,
+  description,
+  headline,
+  lightText,
+  topLine,
+  img,
+  alt,
+  start,
+}) => {
   return (
     <>
       <InfoSec lightBg={lightBg}>
         <Container>
           <InfoRow imgStart={imgStart}>
             <InfoColumn>
-              <TextWrapper></TextWrapper>
+              <TextWrapper>
+                <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <SubTitle lightTextDesc={lightTextDesc}>{description}</SubTitle>
+                <Link to="/sign-up">
+                  <Button big fontBig primary>
+                    {buttonLabel}
+                  </Button>
+                </Link>
+              </TextWrapper>
+            </InfoColumn>
+            <InfoColumn>
+              <ImgWrapper start={start}>
+                <Img src={img} alt={alt} />
+              </ImgWrapper>
             </InfoColumn>
           </InfoRow>
         </Container>
